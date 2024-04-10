c1c6f07c46b79c5cb0352f5f7a478e993475a0be
rooot <hey@rooot.gay>
2024-04-10 08:40:14 +0200

    bleh
    Signed-off-by: rooot <hey@rooot.gay>

--- assets/script/idk.js
+++ assets/script/idk.js
@@ -163,38 +163,3 @@ function update_ui() {
     document.getElementById("form_"+key).value = profile_data[key];
   }
 }
-
-/*
-  document.getElementById('profileForm').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent default form submission
-
-    // Collect form data
-    var formData = {
-      pronoun1: document.getElementById('pronoun.1').value,
-      pronoun2: document.getElementById('pronoun.2').value,
-      pronoun3: document.getElementById('pronoun.3').value,
-      discord: document.getElementById('discord').value,
-      // Collect other form fields similarly
-    };
-
-    // Convert form data to JSON
-    var jsonData = JSON.stringify(formData);
-
-    // Send JSON data to server
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'http://172.27.40.146:82', true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === XMLHttpRequest.DONE) {
-        if (xhr.status === 200) {
-          console.log('Data sent successfully');
-          // Handle success
-        } else {
-          console.error('Error sending data');
-          // Handle error
-        }
-      }
-    };
-    xhr.send(jsonData);
-  });
-  */
